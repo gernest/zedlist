3f75b43d0cc0f20421616c2d16bd008489cb9f25
gernest <geofreyernest@live.com>
2015-08-08 17:57:03 +0300

    updated godoc

---
+++ routes/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2015 Geofrey Ernest <geofreyernest@live.com>. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package routes is a collection of zedlist routes handlers. They are all based on echo library.
+package routes
