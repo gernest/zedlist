81e0d5142f0a2c4540879e27d137568ec4517e7e
gernest <geofreyernest@live.com>
2015-08-08 19:07:21 +0300

    moved the zedlist command to cmd package

---
+++ cmd/zedlist/zedlist.go
@@ -0,0 +1,27 @@
+// Copyright 2015 Geofrey Ernest <geofreyernest@live.com>. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"os"
+
+	"github.com/gernest/zedlist/modules/version"
+
+	"github.com/codegangsta/cli"
+	"github.com/gernest/zedlist/cmd/app"
+)
+
+func main() {
+	zedApp := cli.NewApp()
+	zedApp.Name = "Zedlist"
+	zedApp.Usage = "A job recruitment service"
+	zedApp.Version = version.VERSION
+	zedApp.Authors = app.Authors
+	zedApp.Commands = []cli.Command{
+		app.ServerCommand,
+		app.MigrateCommand,
+	}
+	zedApp.Run(os.Args)
+}
