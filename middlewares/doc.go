a4b2b630790374c8b985f2cfb26a4f32e62d4c84
gernest <geofreyernest@live.com>
2015-08-08 17:57:03 +0300

    updated godoc

---
+++ middlewares/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2015 Geofrey Ernest <geofreyernest@live.com>. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package middlewares contains middlewares for zedlist. They are all based on echo library.
+package middlewares
---
+++ modules/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2015 Geofrey Ernest <geofreyernest@live.com>. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package modules is a collection of zedlist modules.
+package modules
