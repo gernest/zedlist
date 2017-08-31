bd89d4b58a005d3f610c6737d8e237a1d6d11d3e
gernest <geofreyernest@live.com>
2017-08-31 20:50:09 +0300

    Make username unique

--- models/models.go
+++ models/models.go
@@ -190,7 +190,7 @@ type User struct {
 	ID        int
 	Email     string `sql:"type:varchar(100);unique_index"`
 	Password  string
-	Name      string
+	Name      string `gorm:"unique"`
 	Person    Person
 	PersonID  int
 	Status    int
