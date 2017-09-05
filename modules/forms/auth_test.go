ffc47934936a02f49833eafcfa5334702c6314d6
gernest <geofreyernest@live.com>
2017-09-05 19:23:03 +0300

    Delete absolete code

--- modules/forms/auth_test.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2015 Geofrey Ernest <geofreyernest@live.com>. All rights reserved.
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
-
-package forms
