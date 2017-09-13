f279db982e518d2406fb96f3d950365331fc2d17
gernest <geofreyernest@live.com>
2017-09-13 17:20:58 +0300

    Initial Basic component

--- static/js/resume.js
+++ static/js/resume.js
@@ -20,9 +20,11 @@ class HTTP {
 
 
 const profileId = parseInt(localStorage.getItem('profileID'))
+const state = {};
+const bus = new Moon();
 
 Moon.component("resume", {
-    template: `<div>
+    template: `<div class="row nice-box">
     <h2 class="ui dividing header"> create a new resume </h2>
 <div m-if="hasMessage" class="ui transition message {{messageState}}">
     <div class="header">{{ messageText}}</div>
@@ -33,7 +35,7 @@ Moon.component("resume", {
             <label>title</label>
             <input type="text" name="title" m-on:change="updateTitle(event)">
         </div>
-        <button class="ui fluid large submit button" m-on:click.prevent="create(event)"> {{action}} </button>
+        <button class="ui fluid large primary submit button" m-on:click.prevent="create(event)"> {{action}} </button>
     </form>
 </div>
     `,
@@ -95,6 +97,8 @@ Moon.component("resume", {
                 this.set('created', true)
                 this.set('value', data)
                 this.set('action', 'Update')
+                state.resume = data;
+                bus.emit('createResume', data)
             } else if (err) {
                 this.callMethod('message', ['error', 'some fish'])
             }
@@ -102,7 +106,101 @@ Moon.component("resume", {
     },
 })
 
+Moon.component('basic', {
+    template: `<div class="row nice-box">
+    <h2 class="ui dividing header"> Basic details </h2>
+<div m-if="hasMessage" class="ui transition message {{messageState}}">
+    <div class="header">{{ messageText}}</div>
+    <i class="close icon" m-on:click="destroyMessage"></i>
+ </div>
+    <form method="post" class="ui large form">
+        <div class="field">
+            <label>Name</label>
+            <input type="text" name="name" m-on:change="updateField(event)">
+        </div>
+        <div class="field">
+            <label>Label</label>
+            <input type="text" name="label" m-on:change="updateField(event)">
+        </div>
+        <!-------
+        <div class="field">
+            <label>Picture</label>
+            <input type="file" name="picture" m-on:change="updateField(event)">
+        </div>
+        ---->
+        <div class="field">
+            <label>Email</label>
+            <input type="email" name="email" m-on:change="updateField(event)">
+        </div>
+        <div class="field">
+            <label>Phone</label>
+            <input type="tel" name="phone" m-on:change="updateField(event)">
+        </div>
+        <div class="field">
+            <label>Summary</label>
+            <input type="text" name="summary" m-on:change="updateField(event)">
+        </div>
+        <button class="ui fluid large primary submit button" m-on:click.prevent="create(event)"> {{action}} </button>
+    </form>
+</div>`,
+    data: function () {
+        return {
+            fields: {
+                name: '',
+                label: '',
+                picture: '',
+                email: '',
+                phone: '',
+                summary: ''
+            },
+            hasMessage: false,
+            messageState: '',
+            messageText: '',
+            created: false,
+            value: {},
+            action: 'Create'
+        }
+    },
+    methods: {
+        updateField(e) {
+            this.set(`fields.${e.target.name}`, e.target.value)
+        },
+        message(state, text) {
+            this.set('hasMessage', true)
+            this.set('messageState', state)
+            this.set('messageText', text)
+        },
+        destroyMessage() {
+            this.set('hasMessage', false)
+        },
+        create(e) {
+            const value = this.get("fields")
+        }
+    }
+})
+
+Moon.component('app', {
+    template: `<div>
+    <resume></resume>
+    <basic m-if="showBasic"></basic>
+    </div>
+    `,
+    data: function () {
+        return {
+            showBasic: false,
+        }
+    },
+    hooks: {
+        init() {
+            const self = this;
+            bus.on("createResume", (payload) => {
+                self.set('showBasic', true)
+                state.resume=payload;
+            })
+        }
+    }
+})
 
 const app = new Moon({
-    el: "#app",
+    el: "#app"
 });
